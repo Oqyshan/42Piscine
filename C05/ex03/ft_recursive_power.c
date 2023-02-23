e82a1bdc56f95c727f3a258571edaeaeb7823fa6
Oqyshan <123980594+Oqyshan@users.noreply.github.com>
2023-02-23 17:24:11 +0300

    C05 son örnek hariç yüklenmiştir.

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ounal <ounal@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 14:16:40 by ounal             #+#    #+#             */
+/*   Updated: 2023/02/21 14:36:12 by ounal            ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	int	result;
+
+	result = nb;
+	if (power > 1)
+		return (nb * ft_recursive_power(nb, (power - 1)));
+	if (power == 0)
+		return (1);
+	if (power < 0)
+		return (0);
+	return (result);
+}
---
+++ C05/ex04/ft_fibonacci.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ounal <ounal@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 14:39:07 by ounal             #+#    #+#             */
+/*   Updated: 2023/02/21 14:42:46 by ounal            ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index < 2)
+		return (index);
+	return (ft_fibonacci(index - 2) + ft_fibonacci(index - 1));
+}
